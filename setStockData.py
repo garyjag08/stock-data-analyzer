689b0d780802d78b3e53ae07375cd9aeef8c9fbc
garyjag08 <48196744+garyjag08@users.noreply.github.com>
2024-12-13 13:48:28 -0800

    Update setStockData.py
    added method to return list of all closing prices

--- setStockData.py
+++ setStockData.py
@@ -18,3 +18,31 @@ class SetStockData:
 '''
   def set_stock_data(self):
     return yf.download(self.ticker, start=self.start_date, end=self.end_date) # returns the dataframe of stock data
+
+'''
+ * Method to get closing prices of a stock
+ * This method gets a stock prices from Yahoo Finance and stores them as a dataframe
+ * The closing prices are then stored in a list and returned
+ * If a symbol is invalid then the invalid message is returned
+'''
+def getClosingPrices(stock, start, end):
+    '''
+    ARGS:
+      stock symbol
+      start date
+      end date
+      returns a list of closing prices
+    '''
+    # Start and Ending dates for stock data (format of YYYY-MM-DD)
+    start_date = start
+    end_date = end
+    
+    stock_data = SetStockData(stock, start_date, end_date)
+    stock_df = stock_data.set_stock_data()
+    
+    values = []
+    for i in range(len(stock_df)):
+        values.append(stock_df.iloc[i]["Close"].values[0])
+    if len(values) == 0:
+        return "Sorry there was an error, please enter a valid ticker symbol" # invalid ticker symbol
+    return values
